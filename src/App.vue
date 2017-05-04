242ef01b7371bc996c5e9e60aae5eb2456baa7bb
Ryan Hightower <ryan@ryanhightower.com>
2017-05-04 16:03:52 -0600

    Initial commit
    Sample Vue project to display simple crud operations.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <user-display></user-display>
+  </div>
+</template>
+
+<script>
+import UserDisplay from './components/UserDisplay'
+
+export default {
+  name: 'app',
+  components: {
+    UserDisplay
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/UserDisplay.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <button @click="getUsers">Get Users</button>
+  <pre>{{users}}</pre>
+</div>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  data () {
+    return {
+      users: {}
+    }
+  },
+  mounted () {
+    // This lifecycle hook could be used to mount users on load.
+    // Axios.get('//yoursco-web-api.herokuapp.com/users').then((res) => {
+    //   console.log(res.data)
+    //   this.users = res.data
+    // })
+  },
+  methods: {
+    getUsers () {
+      Axios.get('//yoursco-web-api.herokuapp.com/users').then((res) => {
+        console.log(res.data)
+        this.users = res.data
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
